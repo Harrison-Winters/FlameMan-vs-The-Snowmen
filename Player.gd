f4cd891af4acfc5d0b1ce85ea0edaa4e27977360
Harrison-Winters <winters23@up.edu>
2022-10-10 18:11:39 -0700

    Movement Relative to Camera
    Added tests cylinders

--- Player.gd
+++ Player.gd
@@ -1,26 +1,38 @@
 extends KinematicBody
 
 signal hit
-export var speed  = 14
 export var jump_impulse = 20.0
 export var fall_acceleration = 75
 var bounce_impulse = 16.0
+export var gravity_multiplier = 3.0
+export var speed = 10
+export var acceleration = 8
+export var deceleration = 10
+var direction = Vector3()
+var input_axis = Vector2()
+var velocity = Vector3()
+var snap = Vector3()
+var up_direction = Vector3.UP
 
-var velocity = Vector3.ZERO
+
 
 func _physics_process(delta):
-#	$pivot.look_at(get_viewport().get_mouse_position(),Vector3.UP)
+	input_axis = Input.get_vector("move_back", "move_forward",
+			"move_left", "move_right")
 	
-	var direction = Vector3.ZERO # direction starts out at 0
-	# these are functions for controling the characters walking movements
-	if Input.is_action_pressed("move_right"):
-		direction.x +=1
-	if Input.is_action_pressed("move_left"):
-		direction.x -=1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -=1
-	if Input.is_action_pressed("move_back"):
-		direction.z +=1
+	var direction = Vector3.ZERO 
+	var aim: Basis = get_global_transform().basis
+
+	if input_axis.x >= 0.5:
+		direction -= aim.z
+	if input_axis.x <= -0.5:
+		direction += aim.z
+	if input_axis.y <= -0.5:
+		direction -= aim.x
+	if input_axis.y >= 0.5:
+		direction += aim.x
+	direction.y = 0
+	direction = direction.normalized()
 	
 	if Input.is_action_pressed("mouse_escape"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
